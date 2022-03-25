d73fda161140b004a2d68e4ac7e3b47b076f9967
Jake2206 <jla2206@columbia.edu>
2022-03-24 20:05:30 -0400

    Test suite set up and intital test cases covering type creation.

--- tests/scanner_tests.ml
+++
@@ -1,9 +0,0 @@
-(* open OUnit2 *)
-open Src
-
-let _ =
-  let lexbuf = Lexing.from_channel stdin in
-  let program = Commaparse.program_rule Scanner.tokenize lexbuf in
-  print_endline (Ast.string_of_program program)
-
-
