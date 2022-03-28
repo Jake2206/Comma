43924eca91b521b5a64483aed1410576b6b816c7
Etesam913 <etesamansari13@gmail.com>
2022-03-28 15:13:27 -0400

    ✅ Added more tests for function_decl

--- tests/test_suite.ml
+++ tests/test_suite.ml
@@ -24,24 +24,35 @@ let parse_tests = "Test suite for parser" >::: [
                   run_fail_test "int decl w/ assign illegal" "int a = &" (Failure("illegal character &"));
                   run_fail_test "int decl w/ illegal var name1" "int 4;" Stdlib.Parsing.Parse_error;
                   run_fail_test "int decl w/ illegal var name2" "int double;" Stdlib.Parsing.Parse_error;
+
                   run_pass_test "bool dclr w/ assign" "bool a = true;" "bool a = true;\n"; 
                   run_fail_test "bool decl w/ assign illegal" "bool a = )" Stdlib.Parsing.Parse_error;
                   run_fail_test "bool decl w/ illegal var name" "bool double;" Stdlib.Parsing.Parse_error;
+
                   run_pass_test "if expr w/ empty stmt" "if (true) { }" "if (true)\n{\n}\nelse\n{\n}\n";
                   run_fail_test "if expr w/ empty stmt" "if (true;) { }" Stdlib.Parsing.Parse_error;
                   run_pass_test "if expr w/ eif" "if (true) { } eif (false) {}" "if (true)\n{\n}\nelse\nif (false)\n{\n}\nelse\n{\n}\n";
+
                   run_pass_test "double decl w/ assign" "double b = 3.2;" "double b = 3.2;\n"; 
                   run_fail_test "double decl w/ illegal var name1" "double %;" (Failure("illegal character %"));
                   run_fail_test "double decl w/ illegal var name2" "double int" Stdlib.Parsing.Parse_error;
+
                   run_pass_test "char decl" "char b = 'a';" "char b = 'a';\n"; 
                   run_fail_test "char decl w/ illegal var name1" "char %;" (Failure("illegal character %"));
                   run_fail_test "char decl w/ illegal var name2" "char int" Stdlib.Parsing.Parse_error;
+
                   run_pass_test "list decl w/ assign" "int[] b = [3, 4];" "int b = [34];\n";
                   run_pass_test "list decl w/ assign" "int[] b = [3, 't'];" "int b = [3't'];\n";
                   run_fail_test "list decl w/ illegal assign" "bool[] %;" (Failure("illegal character %"));
+                  run_fail_test "list decl w/ illegal var name" "int[] int" Stdlib.Parsing.Parse_error;
+
                   run_pass_test "func declr" "def int test_f ( int a, int b ) { return a+b; }" "def int test_f(int a, int b)\n{\nreturn a + b;\n}\n";
                   run_fail_test "func declr w/ no types in parameters" "def int test_f ( a, b ) { return a+b; }" Stdlib.Parsing.Parse_error;
                   run_fail_test "func declr w/ no return type" "def test_f ( a, b ) { return a+b; }" Stdlib.Parsing.Parse_error;
-                  run_fail_test "list decl w/ illegal var name" "int[] int" Stdlib.Parsing.Parse_error;
+                  run_pass_test "func declr w/ vars" "def int test_f ( int a, int b ) { int c = 9; return a+b+c; }" "def int test_f(int a, int b)\n{\nint c = 9;\nreturn a + b + c;\n}\n";
+                  run_pass_test "func declr w/ if stmt" "def int test_f ( int a, int b ) { int c = 9; if(a==b){return a;}else{return c;}}" "def int test_f(int a, int b)\n{\nint c = 9;\nif (a == b)\n{\nreturn a;\n}\nelse\n{\nreturn c;\n}\n}\n";
+                  run_pass_test "func declr w/ while stmt" "def int test_f ( int a, int b ) { int c = 9; while(a>b){a=a-1;}return a;}" "def int test_f(int a, int b)\n{\nint c = 9;\nwhile (a > b) {\na = a - 1;\n}\nreturn a;\n}\n";
+                  run_pass_test "local variable declr and func declr w/ if stmt" "int g = 10; def int test_f ( int a, int b ) { int c = 9; if(a==b){return a;}else{return c;}}" "int g = 10;\ndef int test_f(int a, int b)\n{\nint c = 9;\nif (a == b)\n{\nreturn a;\n}\nelse\n{\nreturn c;\n}\n}\n";
+                  
 ]
 let _ = run_test_tt_main parse_tests
