af7d03af94c81c49b73dcecb0d3c5b5d7fde44c5
Elysia Witham <e.witham@columbia.edu>
2022-05-08 16:40:46 -0400

    added script to automate testing of comma text files

---
+++ tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+dockerd &> /dev/null &
+
+RC=$(./run.sh)
+
+eval $(opam env)
+
+cd src
+
+ocamlbuild -pkgs llvm llvm.analysis comma.native
+
+make
+
+for TESTFILE in ./test_files/*
+do 
+  ./comma.native -l $TESTFILE > IRoutfile.ll
+  llc -relocation-model=pic IRoutfile.ll > IRoutfile.s
+  cc -o IRoutfile.exe IRoutfile.s libstd.o
+  RC=$(./IRoutfile.exe)
+  if [ $RC != 0 ]
+  then
+    echo "Executable exited with exit code $RC"
+    exit 1
+  fi
+done
+
+cd ..
